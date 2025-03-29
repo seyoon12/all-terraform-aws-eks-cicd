39072fffc408fd4c902ff9f90b5e17c750f03e32
seyoon12 <138763053+seyoon12@users.noreply.github.com>
2025-03-29 21:10:00 +0900

    Delete output.tf

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "k8s_private_key" {
-  value     = module.keypair.k8s_private_key
-  sensitive = true
-}
-
-output "github_oauth_token" {
-  value = var.github_oauth_token
-  sensitive = true
-}
